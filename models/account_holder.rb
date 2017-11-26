3e8dfb3a0c71b52ea21854702988c575ce67b9cc
DeepCFoxXx <learncode23@gmail.com>
2017-11-26 15:55:37 +0000

    Saves done in models

--- models/account_holder.rb
+++ models/account_holder.rb
@@ -1,11 +1,32 @@
 require_relative('../db/sql_runner.rb')
+require_relative('./transaction.rb')
 
 class AccountHolder
 
- attr_reader
+ attr_reader :id, :name, :funds, :transaction_id
 
- def initialize
+ def initialize(options)
+  @id = options['id'].to_i
+  @name = options['name']
+  @funds = options['funds'].to_s
+  @transaction_id = options['transaction_id'].to_i
+ end
 
+ def save()
+   sql = "INSERT INTO account_holders
+   (
+    name,
+    funds,
+    transaction_id
+    )
+    values
+    (
+    $1, $2, $3
+    )
+    RETURNING *"
+    values = [@name, @funds, @tag, @transaction_id]
+    account_holder_data = SqlRunner.run(sql,values)
+    @id = account_holder_data.first()['id'].to_i
  end
 
 end
--- models/transaction.rb
+++ models/transaction.rb
@@ -1,11 +1,34 @@
 require_relative('../db/sql_runner.rb')
+require_relative('./account_holder.rb')
 
 class Transaction
 
- attr_reader
+ attr_reader :id, :merchant_name, :value, :tag, :account_holder_id
 
- def initialize
-   
+ def initialize(options)
+  @id = options['id'].to_i
+  @merchant_name = options['merchant_name']
+  @value = options['value'].to_s
+  @tag = options['options']
+  @account_holder_id = options['account_holder_id'].to_i
+ end
+
+ def save()
+   sql = "INSERT INTO transactions
+   (
+    merchant_name,
+    value,
+    tag,
+    account_holder_id
+    )
+    values
+    (
+    $1, $2, $3, $4
+    )
+    RETURNING *"
+    values = [@merchant_name, @value, @tag, @account_holder_id]
+    transaction_data = SqlRunner.run(sql,values)
+    @id = transaction_data.first()['id'].to_i
  end
 
 end
